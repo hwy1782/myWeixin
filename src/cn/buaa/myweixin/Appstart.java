730b62743293cd9efd7fd1a33e5494e6de2649d5
hwy1782 <hwy1782@gmail.com>
2013-04-25 17:00:06 +0800

    第一次提交
    递交一些基本的资源文件 和 java文件

---
+++ src/cn/buaa/myweixin/Appstart.java
@@ -0,0 +1,32 @@
+package cn.buaa.myweixin;
+
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Appstart extends Activity{
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);	
+		setContentView(R.layout.appstart);
+		//requestWindowFeature(Window.FEATURE_NO_TITLE);//去掉标题栏
+		//getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+        //        WindowManager.LayoutParams.FLAG_FULLSCREEN);   //全屏显示
+		//Toast.makeText(getApplicationContext(), "孩子！好好背诵！", Toast.LENGTH_LONG).show();
+		//overridePendingTransition(R.anim.hyperspace_in, R.anim.hyperspace_out);
+		
+	new Handler().postDelayed(new Runnable(){
+		@Override
+		public void run(){
+			Intent intent = new Intent (Appstart.this,Welcome.class);			
+			startActivity(intent);			
+			Appstart.this.finish();
+		}
+	}, 1000);
+   }
+}
